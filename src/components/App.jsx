b3141be53b8ce1017f9e06543741500c181b30be
Alex1s-C <altcardonalexis@gmail.com>
2021-05-08 10:23:25 +0200

    update working exo1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/header.jsx"
-import Footer from "./components/footer.jsx"
-import Note from "./components/note.jsx";
+import Header from "./header"
+import Footer from "./footer"
+import Note from "./note";
 
 function App() {
   return (
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../App.css';
+import './App.css';
 
 const year = new Date().getFullYear();
 
